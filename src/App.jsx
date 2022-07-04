faefa9ea26418aad5c6f71103e5525f5ee5b76c9
huthoinguyn <huthoi99@gmail.com>
2022-07-04 22:00:32 +0700

    design trello

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,146 @@
-import './App.css'
+import "./App.scss";
 
 function App() {
-
   return (
     <div className="App">
-      Hello world
+      <div className="navbar app">app</div>
+      <div className="navbar board">board</div>
+      <div className="board-columns">
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src="https://scontent.fpnh22-1.fna.fbcdn.net/v/t39.30808-6/269718825_297116925684414_1411415654089061183_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=e3f864&_nc_ohc=cvhMUtkSn6wAX8tba4D&_nc_ht=scontent.fpnh22-1.fna&oh=00_AT-OZj9s2dRhXtDRe89cHSFVmeg86IM81sdzEyBdhuV9bQ&oe=62C79BF9"
+                alt="huthoine-img"
+              />
+              Title: Huthoine
+            </li>
+            <li>Add what you like'd to work on below</li>
+            <li>Add what you like'd to work on below</li>
+            <li>Add what you like'd to work on below</li>
+            <li>Add what you like'd to work on below</li>
+            <li>Add what you like'd to work on below</li>
+            <li>Add what you like'd to work on below</li>
+            <li>Add what you like'd to work on below</li>
+            <li>Add what you like'd to work on below</li>
+            <li>Add what you like'd to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src="https://scontent.fpnh22-1.fna.fbcdn.net/v/t39.30808-6/269718825_297116925684414_1411415654089061183_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=e3f864&_nc_ohc=cvhMUtkSn6wAX8tba4D&_nc_ht=scontent.fpnh22-1.fna&oh=00_AT-OZj9s2dRhXtDRe89cHSFVmeg86IM81sdzEyBdhuV9bQ&oe=62C79BF9"
+                alt="huthoine-img"
+              />
+              Title: Huthoine
+            </li>
+            <li>Add what you like'd to work on below</li>
+            <li>Add what you like'd to work on below</li>
+            <li>Add what you like'd to work on below</li>
+            <li>Add what you like'd to work on below</li>
+            <li>Add what you like'd to work on below</li>
+            <li>Add what you like'd to work on below</li>
+            <li>Add what you like'd to work on below</li>
+            <li>Add what you like'd to work on below</li>
+            <li>Add what you like'd to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src="https://scontent.fpnh22-1.fna.fbcdn.net/v/t39.30808-6/269718825_297116925684414_1411415654089061183_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=e3f864&_nc_ohc=cvhMUtkSn6wAX8tba4D&_nc_ht=scontent.fpnh22-1.fna&oh=00_AT-OZj9s2dRhXtDRe89cHSFVmeg86IM81sdzEyBdhuV9bQ&oe=62C79BF9"
+                alt="huthoine-img"
+              />
+              Title: Huthoine
+            </li>
+            <li>Add what you like'd to work on below</li>
+            <li>Add what you like'd to work on below</li>
+            <li>Add what you like'd to work on below</li>
+            <li>Add what you like'd to work on below</li>
+            <li>Add what you like'd to work on below</li>
+            <li>Add what you like'd to work on below</li>
+            <li>Add what you like'd to work on below</li>
+            <li>Add what you like'd to work on below</li>
+            <li>Add what you like'd to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src="https://scontent.fpnh22-1.fna.fbcdn.net/v/t39.30808-6/269718825_297116925684414_1411415654089061183_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=e3f864&_nc_ohc=cvhMUtkSn6wAX8tba4D&_nc_ht=scontent.fpnh22-1.fna&oh=00_AT-OZj9s2dRhXtDRe89cHSFVmeg86IM81sdzEyBdhuV9bQ&oe=62C79BF9"
+                alt="huthoine-img"
+              />
+              Title: Huthoine
+            </li>
+            <li>Add what you like'd to work on below</li>
+            <li>Add what you like'd to work on below</li>
+            <li>Add what you like'd to work on below</li>
+            <li>Add what you like'd to work on below</li>
+            <li>Add what you like'd to work on below</li>
+            <li>Add what you like'd to work on below</li>
+            <li>Add what you like'd to work on below</li>
+            <li>Add what you like'd to work on below</li>
+            <li>Add what you like'd to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src="https://scontent.fpnh22-1.fna.fbcdn.net/v/t39.30808-6/269718825_297116925684414_1411415654089061183_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=e3f864&_nc_ohc=cvhMUtkSn6wAX8tba4D&_nc_ht=scontent.fpnh22-1.fna&oh=00_AT-OZj9s2dRhXtDRe89cHSFVmeg86IM81sdzEyBdhuV9bQ&oe=62C79BF9"
+                alt="huthoine-img"
+              />
+              Title: Huthoine
+            </li>
+            <li>Add what you like'd to work on below</li>
+            <li>Add what you like'd to work on below</li>
+            <li>Add what you like'd to work on below</li>
+            <li>Add what you like'd to work on below</li>
+            <li>Add what you like'd to work on below</li>
+            <li>Add what you like'd to work on below</li>
+            <li>Add what you like'd to work on below</li>
+            <li>Add what you like'd to work on below</li>
+            <li>Add what you like'd to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src="https://scontent.fpnh22-1.fna.fbcdn.net/v/t39.30808-6/269718825_297116925684414_1411415654089061183_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=e3f864&_nc_ohc=cvhMUtkSn6wAX8tba4D&_nc_ht=scontent.fpnh22-1.fna&oh=00_AT-OZj9s2dRhXtDRe89cHSFVmeg86IM81sdzEyBdhuV9bQ&oe=62C79BF9"
+                alt="huthoine-img"
+              />
+              Title: Huthoine
+            </li>
+            <li>Add what you like'd to work on below</li>
+            <li>Add what you like'd to work on below</li>
+            <li>Add what you like'd to work on below</li>
+            <li>Add what you like'd to work on below</li>
+            <li>Add what you like'd to work on below</li>
+            <li>Add what you like'd to work on below</li>
+            <li>Add what you like'd to work on below</li>
+            <li>Add what you like'd to work on below</li>
+            <li>Add what you like'd to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
